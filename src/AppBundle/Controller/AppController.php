1c730e8301062e5984821fbb021e3137bf3a3c59
Youssouf SEKHARI <youssouf.sekhari@kabolt.fr>
2018-06-13 20:27:50 +0200

    adds index, controller, menu && route

---
+++ src/AppBundle/Controller/AppController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\controller;
+
+
+use symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+
+
+class AppController extends Controller {
+
+  public function indexAction(Request $request) {
+    
+  }
+}
+ ?>
