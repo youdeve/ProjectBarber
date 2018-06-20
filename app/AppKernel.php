a991fae0987740274f65acb8039b3de942f9e15f
Youssouf SEKHARI <youssouf.sekhari@kabolt.fr>
2018-06-20 14:58:56 +0200

    adds FrontBundle and BackBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,11 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+
+            //Bundle
             new AppBundle\AppBundle(),
+            new BackBundle\BackBundle(),
+            new FrontBundle\FrontBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
