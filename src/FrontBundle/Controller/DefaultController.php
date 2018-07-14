36e2a19ecf99899529dee14848666384c08583df
Youssouf SEKHARI <youssouf.sekhari@kabolt.fr>
2018-07-14 16:52:55 +0200

    adds folder public in ressource CSS, js..

--- src/FrontBundle/Controller/DefaultController.php
+++ src/FrontBundle/Controller/DefaultController.php
@@ -11,21 +11,13 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
 
-        /**
-         * @Route("/user", name="front_homepage")
-         * @Security("has_role('ROLE_FRONT_ACCESS' ) ")
-         */
-        public function indexAction(Request $request)
-        {
-          $user = $this->getUser();
-            if($this->getUser()->hasRole('ROLE_CLIENT')) {
-              return $this->render('@Front/default/index-client.html.twig');
-            }else{
-              return $this->redirect($this->generateUrl('fos_user_security_login'));
-            }
-
-        }
-
-
+  /**
+   * @Route("/user", name="front_homepage")
+   * @Security("has_role('ROLE_FRONT_ACCESS' ) ")
+   */
+  public function indexAction(Request $request)
+  {
+    return $this->render('@Front/Default/index-client.html.twig');
+  }
 
 }
