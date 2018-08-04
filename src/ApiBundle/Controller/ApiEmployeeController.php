4037e104fce89fe5a12ff3681d2ce0db66862732
Youssouf SEKHARI <youssouf.sekhari@kabolt.fr>
2018-08-04 20:03:21 +0200

    adds fos_route, config fos route, show api employee

---
+++ src/ApiBundle/Controller/ApiEmployeeController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ApiBundle\Controller;
+
+use AppBundle\Entity\User;
+use FOS\RestBundle\Controller\FOSRestController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use FOS\RestBundle\View\ViewHandler;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\Get;
+
+
+
+class ApiEmployeeController extends FOSRestController
+{
+
+  /**
+   * @Rest\Get("/employees")
+   * @Security("has_role('ROLE_ADD_ADMIN')")
+   * @param Request $request
+   * @return Response
+   */
+  public function getEmployeesAction(Request $request)
+  {
+    try {
+        $employees = $this->getDoctrine()->getRepository("AppBundle:User")->findAll();
+
+        $view = View::create($employees);
+       $view->setFormat('json');
+       if($view == null)
+       return new JsonResponse(['message' => 'Les utilisateurs n\'existes pas'], Response::HTTP_UNPROCESSABLE_ENTITY);
+
+       return $view;
+
+    } catch (\Exception $e) {
+      return new View([$e->getMessage()], Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+  }
+
+}
